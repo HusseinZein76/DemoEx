b814bffa2d178190e924ebed59462b9460615a11
Hussein Zein <husseinzein76@outlook.com>
2022-09-01 17:59:19 +0300

    Create .env file and pass it for orm.config

--- apps/service1/src/app/orm.config.ts
+++ apps/service1/src/app/orm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const config: TypeOrmModuleOptions = {
   type: "postgres",
-  username: "postgres",
-  password: "1234",
-  port: 5432,
-  host: "127.0.0.1",
-  database: "ProductsDemo",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: +process.env.PORT,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
   synchronize: true,
   entities: ["dist/**/*.entity{.ts,.js}"],
   logging: false,
